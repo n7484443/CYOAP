a7a0edfac9d6f48491e6304a9be66db12b94eb67
n7484443 <n7484443@naver.com>
2021-10-23 18:33:32 +0900

    -1.2.0-alpha

--- lib/src/main/java/cyoap_main/core/JavaFxMain.java
+++ lib/src/main/java/cyoap_main/core/JavaFxMain.java
@@ -19,7 +19,7 @@ import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class JavaFxMain extends Application {
-    public static String version = "1.1.5-alpha";
+    public static String version = "1.2.0-alpha";
 
 	public static JavaFxMain instance;
 	public Stage stage;

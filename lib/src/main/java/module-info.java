08ed853c48989aa30da0b1cbd74a92267158af58
n7484443 <n7484443@naver.com>
2021-12-25 03:50:35 +0900

    -필요없는 require 제거

--- lib/src/main/java/module-info.java
+++ lib/src/main/java/module-info.java
@@ -1,14 +1,7 @@
 open module cyoap_module {
-	requires java.desktop;
-	requires java.xml;
-
 	requires org.fxmisc.richtext;
 	requires org.apache.commons.io;
 
-	requires com.fasterxml.jackson.annotation;
-	requires com.fasterxml.jackson.databind;
-
-	requires javafx.base;
 	requires javafx.swing;
 	requires javafx.graphics;
 	requires javafx.fxml;
@@ -21,4 +14,7 @@ open module cyoap_module {
 	requires github.api;
 
 	requires MaterialFX;
+
+	requires com.fasterxml.jackson.annotation;
+	requires com.fasterxml.jackson.databind;
 }
